26fb4c16aeb67426026149d8c7a0a9ab13cafba4
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-04-26 10:40:49 -0600

    Complete my first app tutorial

--- app/src/main/java/co/ismapro/myfirstapp/MyActivity.java
+++ app/src/main/java/co/ismapro/myfirstapp/MyActivity.java
@@ -1,12 +1,17 @@
 package co.ismapro.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+    // constant with the key to use in the extra message of the intent start.
+    public final static String EXTRA_MESSAGE = "com.ismapro.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,4 +41,19 @@ public class MyActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        //Create the intent to lunch the Display Message Activity
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        // get the EditText from the activity
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        // get the text of the editText
+        String message = editText.getText().toString();
+        // includes an additional message into the intent
+        intent.putExtra( EXTRA_MESSAGE, message );
+        // starts the new activity
+        startActivity(intent);
+    }
+
 }
